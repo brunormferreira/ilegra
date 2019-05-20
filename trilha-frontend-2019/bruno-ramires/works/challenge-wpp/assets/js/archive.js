abffbe890486ef9d91de3f198a2b8f6e0f8ac674
Bruno Ramires <brunormferreira@hotmail.com>
2019-05-20 15:18:33 -0300

    js added

--- trilha-frontend-2019/bruno-ramires/works/challenge-wpp/assets/js/archive.js
+++ trilha-frontend-2019/bruno-ramires/works/challenge-wpp/assets/js/archive.js
@@ -6,30 +6,114 @@ const users = [
     time: "20:04"
   },
   {
-    name: "Mister X",
-    lastMessage: "Vamos começar a mágica?",
-    image: "assets/img/me.jpeg",
-    time: "20:04"
-  }
+    name: "Pérola",
+    lastMessage: "Meooowww!!!",
+    image: "assets/img/cat.jpg",
+    time: "17:48"
+  },
+  {
+    name: "Cervejeiro",
+    lastMessage: "Teu copo ja está vazio?",
+    image: "assets/img/cerva.jpg",
+    time: "16:32"
+  },
+  {
+    name: "Flash",
+    lastMessage: "Rápido demais?",
+    image: "assets/img/flash.jpg",
+    time: "16:22"
+  },
+  {
+    name: "Lanterna Verde",
+    lastMessage: "Rrsrsrs...",
+    image: "assets/img/lvv.jpeg",
+    time: "15:59"
+  },
+  {
+    name: "Libélula",
+    lastMessage: "Eu sou um super-herói!",
+    image: "assets/img/libelola.jpg",
+    time: "14:10"
+  },
+  {
+    name: "Hulk",
+    lastMessage: "Hulk esmaga!!!!",
+    image: "assets/img/hulk.jpg",
+    time: "12:10"
+  },
+  {
+    name: "Superman",
+    lastMessage: "Vá para longe com essa kryptonita!",
+    image: "assets/img/sm.jpg",
+    time: "11:38"
+  },
+  {
+    name: "Raphael",
+    lastMessage: "Vamos lutar!!!",
+    image: "assets/img/raptn.jpg",
+    time: "11:00"
+  },
+  {
+    name: "Michelangelo",
+    lastMessage: "Eu estou com fome! Hahahaha.",
+    image: "assets/img/michetn.jpg",
+    time: "09:28"
+  },
+  {
+    name: "Donatello",
+    lastMessage: "Fight!!!!",
+    image: "assets/img/donattn.jpg",
+    time: "07:19"
+  },
 ];
+
+// map gera um array de string, e para tirara "," e gerar apenas uma unica string, basta colocar o .join('') no inner html
 
 const listUsers = users.map(function(user) {
   return `
   <div class="contact">
-    <img src="assets/img/donattn.jpg" class="side-panel-img">
+    <img src="${user.image}" class="side-panel-img">
     <div class="contact-preview">
         <div class="contact-text">
-            <h1 class="font-name">Donatello</h1>
-            <p class="font-preview">Fight!!!!</p>
+            <h1 class="font-name">${user.name}</h1>
+            <p class="font-preview">${user.lastMessage}</p>
         </div>
     </div>
     <div class="contact-time">
-        <p>07:19</p>
+      <p>${user.time}</p>
     </div>
     </div>
   `;
 });
 
-listUsers.addEventListener;
+document.getElementById("contacts-list").innerHTML = listUsers.join('');
+
+document
+  .getElementById("input-filter")
+  .addEventListener("keyup", function(event) {
+    const value = event.target.value;
+
+    const filtered = users.filter(user => user.name.toLowerCase().includes(value));
+
+    let html2 = '';
+
+    filtered.forEach(function (user){
+      html2 += `
+      <div class="contact">
+        <img src="${user.image}" class="side-panel-img">
+        <div class="contact-preview">
+            <div class="contact-text">
+                <h1 class="font-name">${user.name}</h1>
+                <p class="font-preview">${user.lastMessage}</p>
+            </div>
+        </div>
+        <div class="contact-time">
+          <p>${user.time}</p>
+        </div>
+        </div>
+      `
+    })
+    document.getElementById('contacts-list').innerHTML = html2;
+  });
 
 console.log(listUsers);
