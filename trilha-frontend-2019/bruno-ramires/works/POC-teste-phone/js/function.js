9e79846002363058c3c367ab5d89cac6ef1a6185
Bruno Ramires <brunormferreira@hotmail.com>
2019-06-04 16:34:32 -0300

    poc

---
+++ trilha-frontend-2019/bruno-ramires/works/POC-teste-phone/js/function.js
@@ -0,0 +1,25 @@
+// keys = ['.,!1', 'abc2'];
+
+arrayList = document.querySelectorAll('.key');
+
+arrayList.forEach(function (button) {
+
+    console.log(button.value);
+});
+// var clicks = 0;
+
+// function bla() {
+//     if (clicks >= event.target.textContent.length) {
+//         clicks = 0;
+//     }
+//     console.log(event.target.textContent[clicks])
+//     clicks++;
+// }
+
+// keys.forEach.addEventListener("onclick", function () {
+
+// })
+
+function bla() {
+    console.log(event);
+}
