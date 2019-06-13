b13cdf124e82cb77cbde78206723ef528f5bc4ed
Bruno Ramires <brunormferreira@hotmail.com>
2019-06-12 23:22:24 -0300

    header-table semi finish, falta dar uma olhada

--- trilha-frontend-2019/bruno-ramires/works/dashboard/src/components/Header-table.vue
+++ trilha-frontend-2019/bruno-ramires/works/dashboard/src/components/Header-table.vue
@@ -1,6 +1,13 @@
 <template>
   <header class="c-header-table">
-    <h3 class="c-header-table__text">Data Table with full features</h3>
+    <h3 class="c-header-table__text-title">Data Table with full features</h3>
+    <span class="c-header-table__text">Show</span>
+    <input type="text" class="c-header-table__input" placeholder="10">
+    <span>entries</span>
+    <div class="c-header-table__container-input-search">
+      <span>Search:</span>
+      <input type="text" class="c-header-table__input-search" placeholder>
+    </div>
   </header>
 </template>
 
@@ -13,7 +20,38 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .c-header-table {
-  height: 54px;
-  padding: 21px;
+  height: 83px;
+  background: #fcfcfc;
+}
+
+.c-header-table__text-title {
+  font-weight: 400;
+  padding: 11px;
+  margin: 0;
+}
+
+.c-header-table__text {
+  margin-left: 12px;
+}
+.c-header-table__input {
+  outline: 0;
+  border-radius: 4px;
+  border: solid 1px #a1afa9;
+  width: 57px;
+  margin: 4px;
+}
+
+.c-header-table__container-input-search {
+  float: right;
+}
+
+.c-header-table__input-search {
+  outline: 0;
+  border-radius: 2px;
+  border: solid 1px #a1afa9;
+  width: 120px;
+  height: 26px;
+  margin-left: 4px;
+  margin-right: 10px;
 }
 </style>
--- trilha-frontend-2019/bruno-ramires/works/dashboard/src/components/Table.vue
+++ trilha-frontend-2019/bruno-ramires/works/dashboard/src/components/Table.vue
@@ -1,6 +1,47 @@
 <template>
-  <div class="c-main-principal-table">
+  <div class="c-main-principal">
     <HeaderTable></HeaderTable>
+    <table class="c-main-principal__table">
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>E-mail</th>
+        <th>Phone</th>
+        <th>Created</th>
+        <th>Specialist</th>
+        <th>Action</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+      <tr>
+        <td>Ernst Handel</td>
+        <td>Roland Mendel</td>
+        <td>Austria</td>
+      </tr>
+      <tr>
+        <td>Island Trading</td>
+        <td>Helen Bennett</td>
+        <td>UK</td>
+      </tr>
+      <tr>
+        <td>Laughing Bacchus Winecellars</td>
+        <td>Yoshi Tannamuri</td>
+        <td>Canada</td>
+      </tr>
+      <tr>
+        <td>Magazzini Alimentari Riuniti</td>
+        <td>Giovanni Rovelli</td>
+        <td>Italy</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -18,8 +59,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.c-main-principal {
+  width: 96%;
+  height: 62vh;
+  margin-left: 24px;
+}
+
 .c-main-principal-table {
-  width: 200px;
-  height: 150px;
+  border-collapse: collapse;
+  width: 100%;
+}
+th {
+  padding: 8px;
+  text-align: left;
+}
+td {
+  border-top: 1px solid #dddddd;
+  border-bottom: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
 }
 </style>
