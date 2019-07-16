798602da3dc0e4df12c7d1e875d571c13de14bca
Bruno Ramires <brunormferreira@hotmail.com>
2019-07-15 22:08:29 -0300

    new experiences with js

---
+++ trilha-frontend-2019/bruno-ramires/works/experiencies-js/js/funcoes.js
@@ -0,0 +1,11 @@
+export function somaExt(a, b){
+  return  parseFloat(a) + parseFloat(b);
+}
+
+export function multiExt(a, b){
+  return (a * b);
+}
+
+export function divExt(a, b){
+  return (a / b);
+}
---
+++ trilha-frontend-2019/bruno-ramires/works/experiencies-js/js/script.js
@@ -0,0 +1,45 @@
+import { somaExt } from './funcoes.js';
+import { multiExt } from './funcoes.js'
+import { divExt } from './funcoes.js'
+
+var form = document.getElementById('inputs');
+var n1 = document.getElementById('num1');
+var n2 = document.getElementById('num2');
+var operation = ["soma", "multi", "div", "lixo", "derivada"];
+var number1, number2;
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  number1 = n1.value;
+  number2 = n2.value;
+
+  operation.forEach(myFunction);
+});
+
+function myFunction(item) {
+    if(item === "soma"){
+      console.log(somaExt(number1, number2));
+    } else if (item === "multi"){
+      console.log(multiExt(number1, number2));
+    } else if (item === "div"){
+      console.log(divExt(number1, number2));
+    }else{
+      Error.prototype.message = `${item} WARNING "valor de operacao invalido!"`;
+      let data = new Date();
+      console.log(data);
+      console.log(Error.prototype.message);
+    }
+}
+
+// function somaExt(a, b){
+//   return  parseFloat(a) + parseFloat(b);
+// }
+
+// function multiExt(a, b){
+//   return (a * b);
+// }
+
+// function divExt(a, b){
+//   return (a / b);
+// }
