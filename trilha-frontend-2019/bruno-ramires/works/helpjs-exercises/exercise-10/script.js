71549ee02757a401ef2ff3218f3ceb892998cbab
Bruno Ramires <brunormferreira@hotmail.com>
2019-07-17 15:01:16 -0300

    helpjs / exercise 10 done

---
+++ trilha-frontend-2019/bruno-ramires/works/helpjs-exercises/exercise-10/script.js
@@ -0,0 +1,6 @@
+const inputValue1 = document.getElementById('soma1');
+const inputValue2 = document.getElementById('soma2');
+const sumButton = document.getElementById('btn-soma');
+let result = document.getElementById('resultado-soma');
+
+sumButton.addEventListener('click', () => result.value = parseInt(inputValue1.value) + parseInt(inputValue2.value));
