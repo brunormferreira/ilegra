47c6e311d73ba7f34f7391701ad6dfa01aa69147
Bruno Ramires <brunormferreira@hotmail.com>
2019-07-17 13:50:09 -0300

    refact little detail exercise 08

--- trilha-frontend-2019/bruno-ramires/works/helpjs-exercises/exercise-08/script.js
+++ trilha-frontend-2019/bruno-ramires/works/helpjs-exercises/exercise-08/script.js
@@ -3,8 +3,5 @@ const resultado = document.getElementById('resultado');
 
 cpf.addEventListener('keyup', () => {
   let value = cpf.value.replace(/[^0-9]/g, "").replace(/^([\d]{3})([\d]{3})?([\d]{3})?([\d]{2})?/, "$1.$2.$3-$4");
-  
-  let valueCpf = cpf.value;
-  valueCpf = value;
-  resultado.innerText = valueCpf;
+  resultado.innerText = value;
 })
