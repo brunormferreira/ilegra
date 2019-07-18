cd498f1cc263b1c4903f2b628a369711b5295fd0
Bruno Ramires <brunormferreira@hotmail.com>
2019-07-18 15:13:16 -0300

    helpjs / exercise 29 done

---
+++ trilha-frontend-2019/bruno-ramires/works/helpjs-exercises/exercise-29/script.js
@@ -0,0 +1,11 @@
+const result = document.getElementById('resultado');
+
+// let i = 0;
+// while (i < 11) {
+//   result.innerHTML += i;
+//   i++;
+// }
+
+for (let n = 0; n <= 10; n++) {
+  result.innerHTML += n;
+}
