0289410b1081e32ecdd6e6a009e12c79283653e4
Bruno Ramires <brunormferreira@hotmail.com>
2019-07-22 15:39:04 -0300

    marvel api / start connection with api

--- trilha-frontend-2019/bruno-ramires/works/marvel-api-vanillajs/js/script.js
+++ trilha-frontend-2019/bruno-ramires/works/marvel-api-vanillajs/js/script.js
@@ -1,1 +1,3 @@
-alert('Hola Marvel')
+const privateKey = "2ee0807f80c971d782a4eaa79bfff312800dd431",
+      publicKey  = "468a011b902c2aa0cd7230939e5000d4",
+      content    = document.getElementById('content');
