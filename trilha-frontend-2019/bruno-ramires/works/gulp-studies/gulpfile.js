eab01052b1b454d103c1c7cd4de15e8fe39ad2bd
Bruno Ramires <brunormferreira@hotmail.com>
2019-07-26 14:50:12 -0300

    studies about gulpfile / tasks, watch and more (need to finish)

--- trilha-frontend-2019/bruno-ramires/works/gulp-studies/gulpfile.js
+++ trilha-frontend-2019/bruno-ramires/works/gulp-studies/gulpfile.js
@@ -2,14 +2,36 @@
 const gulp = require('gulp');
 
 // call the plugin
-const imagemin = require('gulp-imagemin');
+const minifyCSS = require('gulp-minify-css');
+const concat = require('gulp-concat');
+const autoPrefixer = require('gulp-autoprefixer');
 
 gulp.task('reduzir-imagem', () => {
+});
 
-  let diretorioProjeto = "src/imagens/*";
-  let diretorioBuild = "build/imagens";
+gulp.task('styles', () => {
+  let diretoriosCSS = [
+    'src/vendor/fontawesome-free/css/fontawesome.css',
+    'src/vendor/magnific-popup/magnific-popup.css',
+    'src/css/creative.css'
+  ];
 
-  gulp.src(diretorioProjeto)
-    .pipe(imagemin())
-    .pipe(gulp.dest(diretorioBuild))
+  let diretorioBuild = 'build/css';
+
+  gulp.src(diretoriosCSS)
+    .pipe(concat('main.css'))
+    .pipe(autoPrefixer('last 1 version'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(diretorioBuild));
+});
+
+gulp.task('font-awesome', () => {
+  gulp.src('src/vendor/fontawesome-free/webfonts/*')
+    .pipe(gulp.dest('build/fonts'));
+});
+
+
+// *******default need to configure *******
+gulp.task('default', ['reduzir-imagem', 'styles', 'font-awesome'], function() {
+
 });
