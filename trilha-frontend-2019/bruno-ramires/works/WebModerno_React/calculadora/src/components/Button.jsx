54b6e7c1c5977fc782aa8d8747f4590fe88ba753
Bruno Ramires <brunormferreira@hotmail.com>
2019-07-29 15:36:44 -0300

    add-react: button component #2 unfinished

--- trilha-frontend-2019/bruno-ramires/works/WebModerno_React/calculadora/src/components/Button.jsx
+++ trilha-frontend-2019/bruno-ramires/works/WebModerno_React/calculadora/src/components/Button.jsx
@@ -1,5 +1,31 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>
-  <button className="button">{props.label}</button>
+export default props => {
+  let classes = 'button '
+  classes += props.operation ? 'operation' : ''
+  classes += props.double ? 'double' : ''
+  classes += props.double ? 'triple' : ''
+
+  return (
+    <button 
+      onClick={e => props.click && props.click(props.label)}
+      className={classes}>
+      {props.label}
+    </button>
+  )
+}
+
+// *** OTHER MODE TO DO THE SAME THING WITH CLASSES***
+// export default props => {
+//   return (
+//     <button className={`
+//       button
+//       ${props.operation ? 'operation' : ''}
+//       ${props.double ? 'double' : ''}
+//       ${props.triple ? 'triple' : ''}
+//     `}>
+//       {props.label}
+//     </button>
+//   )
+// }
