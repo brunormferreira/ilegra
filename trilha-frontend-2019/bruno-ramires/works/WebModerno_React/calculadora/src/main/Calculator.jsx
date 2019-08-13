e957e0b139fd15165f8bcd6dc115cb3a450d9631
Bruno Ramires <brunormferreira@hotmail.com>
2019-08-13 10:06:47 -0300

    add-react: finish logic calculator #2

--- trilha-frontend-2019/bruno-ramires/works/WebModerno_React/calculadora/src/main/Calculator.jsx
+++ trilha-frontend-2019/bruno-ramires/works/WebModerno_React/calculadora/src/main/Calculator.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-eval */
 import React, { Component } from 'react'
 import './Calculator.css'
 
@@ -25,11 +26,33 @@ export default class Calculator extends Component {
 
   clearMemory() {
     this.setState({ ...initialState })
-    console.log('limpar')
   }
 
   setOperation(operation) {
-    console.log(operation)
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const currentOperation = this.state.operation
+
+      const values = [...this.state.values]
+      try {
+          values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+      } catch (e) {
+          values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+
+    }
   }
 
   addDigit(n) {
@@ -43,13 +66,12 @@ export default class Calculator extends Component {
     const displayValue = currentValue + n
     this.setState({ displayValue, clearDisplay: false })
 
-    if (n === '.') {
+    if (n !== '.') {
       const i = this.state.current
       const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
       values[i] = newValue
       this.setState({ values })
-      console.log(values)
     }
   }
 
